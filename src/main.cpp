622051b7c0294f5a804182a7efc05794fffe6b43
Marina Atkin <marinaatkin@gmail.com>
2024-10-30 13:52:49 -0500

    class work october 30th

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,46 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
 
-const char* vertexShaderSource =
-	"#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"uniform mat4 transform;"
-	"void main()\n"
-	"{\n"
-	"   gl_Position = transform * vec4(aPos, 1.0); "
-	"}\0";
 
-const char * fragmentShaderSource = "version 330 core\n"
-"out vec4 FragColor\n"
+//old code
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 transform;"
 "void main()\n"
 "{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f)"
+"   gl_Position = transform * vec4(aPos, 1.0); "
 "}\0";
 
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
+//working code
+/*
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 transform;"
+"void main()\n"
+"{\n"
+"   gl_Position = transform * vec4(aPos, 1.0); "
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+*/
 
 struct Triangle;
 struct Vert;
